bc4fe8bd3c5e8367aa40e445a07dcdac83cb5d7e
Sneha Shrinivas <Sneha.Shrinivas@godigit.com>
2024-08-20 16:03:52 +0530

    Added:AuthCard to addnote component

--- src/app/services/Auth/auth.guard.ts
+++ src/app/services/Auth/auth.guard.ts
@@ -1,5 +1,27 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree,CanActivateFn } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+Injectable({
+  providedIn: 'root'
+})
+export class authGuard implements CanActivateFn {
+
+  constructor(private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+
+    if(localStorage.getItem('token')){
+      return true;
+    }
+    else{
+      this.router.navigate(['']);
+      return false;
+    }
+  }
+  
+}
